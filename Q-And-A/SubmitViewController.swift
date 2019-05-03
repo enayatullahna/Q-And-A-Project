3409cba27c3fefd58e2f601930468b9114eb3114
Enayatullah Naseri <ena.nas@icloud.com>
2019-05-02 20:01:45 -0700

    Storyboard for AskQuestionController part 2

---
+++ Q-And-A/SubmitViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SubmitViewController.swift
+//  Q-And-A
+//
+//  Created by Enayatullah Naseri on 5/2/19.
+//  Copyright © 2019 Enayatullah Naseri. All rights reserved.
+//
+
+import UIKit
+
+class SubmitViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
