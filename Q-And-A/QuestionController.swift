0ffe6c5a1af94dadd21d7a19e7847ec84696ee18
Enayatullah Naseri <ena.nas@icloud.com>
2019-05-02 19:42:56 -0700

    Created question controller and Story board part 1

---
+++ Q-And-A/QuestionController.swift
@@ -0,0 +1,25 @@
+//
+//  QuestionController.swift
+//  Q-And-A
+//
+//  Created by Enayatullah Naseri on 5/2/19.
+//  Copyright © 2019 Enayatullah Naseri. All rights reserved.
+//
+
+import Foundation
+
+class QuestionController {
+    var questions: [Question] = []
+}
+
+func createQuestion (){
+    
+}
+
+func updateQuestion (){
+    
+}
+
+func deleteQuestion (){
+    
+}
---
+++ Q-And-A/QuestionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuestionTableViewCell.swift
+//  Q-And-A
+//
+//  Created by Enayatullah Naseri on 5/2/19.
+//  Copyright © 2019 Enayatullah Naseri. All rights reserved.
+//
+
+import UIKit
+
+class QuestionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
